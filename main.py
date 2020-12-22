34264e29807d0efc07f9cb195f9e2336bce3acf3
s-i-d-d-i-s <68241942+s-i-d-d-i-s@users.noreply.github.com>
2020-12-22 14:24:02 +0530

    Final Fixes

--- main.py
+++ main.py
@@ -3,29 +3,9 @@ from discord.ext import commands
 import asyncio
 import random
 import os
-import Utils.tasks as tasks
 ## Setup Client
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix= '=', intents = intents)
-
-
-## Source : https://github.com/cheran-senthil/TLE/blob/724adb4410dc28ad0d556b7c675316131139e3c2/tle/util/discord_common.py#L112
-async def presence(bot):
-    await bot.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.listening,
-        name='your commands'))
-    await asyncio.sleep(60)
-
-    @tasks.task(name='OrzUpdate', waiter=tasks.Waiter.fixed_delay(5*60))
-    async def presence_task(_):
-        while True:
-            target = random.choice([
-                member for member in bot.get_all_members()
-            ])
-            await bot.change_presence(activity=discord.Game(
-                name=f'{target.display_name} orz'))
-            await asyncio.sleep(10 * 60)
-    presence_task.start()
 
 
 
@@ -39,7 +19,7 @@ for filename in os.listdir('./cogs'):
 @client.event
 async def on_ready():
 	print("Bot is Ready")
-	asyncio.create_task(presence(client))
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name='your commands'))
 
 
 ## On Member Join
@@ -56,7 +36,7 @@ async def on_member_remove(member):
 
 
 #Add Your Bot Token
-token = ""
+token = "YOURBOTTOKEN"
 
 
 client.run(token)
